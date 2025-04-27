9ed7b9de9170b53d17a2ce8b07c333fd199c9fe2
Kasador <hunterstevenshaw@gmail.com>
2025-04-26 17:05:35 -0700

    Updated Nav menu. Routes setup correctly.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         }
     },[getLocation]) // if the url changes, then reload it, because my header is only loaded once. and then the routes will change the router (URL)
     return (
-        <header className="w-full h-20 p-4 bg-stone-500 fixed top-0 right-0 left-50 m-5 rounded-3xl flex items-center">
+        <header className="w-full h-20 p-4 bg-stone-600 shadow-lg fixed top-0 right-0 left-55 m-5 rounded-3xl flex items-center">
             <h1 className="text-2xl font-bold">{headerTitle}</h1>
         </header>
     )
