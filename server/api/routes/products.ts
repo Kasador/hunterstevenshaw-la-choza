3efb4e9b360f1e030faf8180ee1aa6d2fcd28026
Kasador <hunterstevenshaw@gmail.com>
2025-05-04 14:55:33 -0700

    Removed backend and making new repo.

--- server/api/routes/products.ts
+++
@@ -1,8 +0,0 @@
-// const router = require('express').Router();
-import { Router } from 'express';
-import { getAllProducts } from '../controller/productsController'
-const router = Router();
-
-router.get('/', getAllProducts)
-
-export default router;
