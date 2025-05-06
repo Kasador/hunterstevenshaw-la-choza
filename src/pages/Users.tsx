b968bbeffc940a4b220e6c6e735ad15af7146902
Kasador <hunterstevenshaw@gmail.com>
2025-05-05 18:01:51 -0700

    ⚠️🔥Feat: Need error handling still, but CRUD operations work now!

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -4,6 +4,8 @@ import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import { TiUserAdd } from "react-icons/ti";
 
+// Dev Notes: Need error handling still, but CRUD operations work now!
+
 export default function Users() {
     type User = {
         _id: string;
@@ -11,7 +13,10 @@ export default function Users() {
         role: string;
     };
 
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState<User[]>([]); // array of user data
+    const [modalType, setModalType] = useState<'edit' | 'delete' | 'add' | null>(null); // If true, open specific modal
+    const [selectedUser, setSelectedUser] = useState<User | null>(null); // check what user is selected
+
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -27,20 +32,26 @@ export default function Users() {
         fetchUsers()
     }, [])
 
-    const handleEdit = () => {
-        console.log('Ready to edit!')
-    }
-
-    const handleDelete = () => {
-        console.log('Are you sure you want to delete?')
-    }
-
-    const handleNewUser = () => {
-        console.log('Make a new user!')
-    }
+    const handleEdit = (user: User) => {
+        setSelectedUser(user);
+        setModalType('edit');
+      };
+      
+      const handleDelete = (user: User) => {
+        setSelectedUser(user);
+        setModalType('delete');
+      };
+      
+      const handleNewUser = () => {
+        setSelectedUser(null);
+        setModalType('add');
+      };
+      
 
     console.log(users) // data
     // https://stackoverflow.com/questions/69080597/%C3%97-typeerror-cannot-read-properties-of-undefined-reading-map
+    // https://stackoverflow.com/questions/73699858/why-my-property-does-not-exist-on-type-never/73700310
+    // https://stackoverflow.com/questions/52325814/why-we-are-using-htmlinputelement-in-typescript
     return (
         <section className="text-stone-900">
             {/* <h1 className='font-bold text-2xl p-4'>Total Usuarios</h1> */}
@@ -61,15 +72,149 @@ export default function Users() {
                     {users?.map((user: User) => (
                         <tr key={user._id} className="hover:bg-gray-100">
                             <td className="px-6 py-4 whitespace-nowrap text-sm">{user.username}</td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm capitalize">{user.role}</td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm">{user.role}</td>
                             <td className='flex align-middle px-6 py-4 whitespace-nowrap text-2xl justify-end mr-10'>
-                                <FaEdit className='cursor-pointer hover:text-green-300 text-green-500' onClick={handleEdit} />&nbsp;&nbsp;
-                                <MdDeleteForever className='cursor-pointer hover:text-red-300 text-red-500' onClick={handleDelete} />
+                                {/* <FaEdit className='cursor-pointer hover:text-green-300 text-green-500' onClick={() => handleEdit(user)} />&nbsp;&nbsp;
+                                <MdDeleteForever className='cursor-pointer hover:text-red-300 text-red-500' onClick={() => handleDelete(user)} /> */}
+                                {user.username !== 'admin' ? (
+                                    <>
+                                        <FaEdit
+                                            className="cursor-pointer hover:text-green-300 text-green-500"
+                                            onClick={() => handleEdit(user)}
+                                        />  &nbsp;&nbsp;
+                                        <MdDeleteForever
+                                            className="cursor-pointer hover:text-red-300 text-red-500"
+                                            onClick={() => handleDelete(user)}
+                                        />
+                                    </>
+                                ) : (
+                                    <span className="text-stone-400 text-sm italic">S&#250;per admin</span>
+                                )}
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+            {modalType && ( 
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+                <div className="bg-white p-6 rounded-lg w-[90%] max-w-md shadow-lg">
+                    {/* Delete Modal */}
+                    {modalType === 'delete' && selectedUser && (
+                    <>
+                        <h2 className="text-xl font-bold mb-4">¿Eliminar usuario?</h2>
+                        <p className="mb-4">¿Seguro que deseas eliminar a <strong>{selectedUser.username}</strong>?</p>
+                        <div className="flex justify-end gap-4">
+                        <button onClick={() => setModalType(null)} className="px-4 py-2 bg-gray-300 rounded cursor-pointer">Cancelar</button>
+                        <button
+                            className="px-4 py-2 bg-red-500 text-white rounded cursor-pointer"
+                            onClick={async () => {
+                                await axios.delete(`${import.meta.env.VITE_API_URL}api/users/${selectedUser._id}`);
+                                setUsers(users.filter(user => user._id !== selectedUser._id)); // find selected user
+
+                                setModalType(null); // close modal
+                            }}
+                        >
+                            Eliminar
+                        </button>
+                        </div>
+                    </>
+                    )}
+
+                    {/* Edit Modal */}
+                    {/* Dev Notes:  Thinking of disabling the change of the user's username. If they need to change it, delete it, and make a new one. */}
+                    {modalType === 'edit' && selectedUser && ( // https://www.geeksforgeeks.org/how-to-set-default-value-in-select-using-reactjs/#
+                    <>
+                        <h2 className="text-xl font-bold mb-4">Editar Usuario</h2>
+                        <input
+                            type="text"
+                            className="border p-2 w-full mb-4"
+                            defaultValue={selectedUser.username}
+                            onChange={(e) => setSelectedUser({ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+                                ...selectedUser, // spread operator 
+                                username: e.target.value
+                            })}
+                        />
+                        <select
+                            className="border p-2 w-full mb-4"
+                            defaultValue={selectedUser.role}
+                            onChange={(e) => setSelectedUser({ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+                                ...selectedUser, // spread operator 
+                                role: e.target.value
+                            })}
+                        >
+                            { /* User's Options */ }
+                            <option value="user">user</option>
+                            <option value="admin">admin</option>
+                        </select>
+                        <div className="flex justify-end gap-4">
+                        <button
+                            type="button"
+                            onClick={() => setModalType(null)} // close modal
+                            className="px-4 py-2 bg-gray-300 rounded cursor-pointer"
+                        >
+                            Cancelar
+                        </button>
+                        <button
+                            type="button"
+                            onClick={async () => {
+                                if (!selectedUser) { // for Typescript, but also error handling
+                                    return
+                                }
+
+                                await axios.put(`${import.meta.env.VITE_API_URL}api/users/${selectedUser._id}`, {
+                                    username: selectedUser.username,
+                                    role: selectedUser.role,
+                                });
+
+                                setUsers(
+                                    users.map((user) => // create new array, then 
+                                        user._id === selectedUser._id ? { ...user, username: selectedUser.username, role: selectedUser.role } : user
+                                    ) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
+                                );
+
+                                setModalType(null); // close modal
+                            }}
+                            className="px-4 py-2 bg-green-600 text-white rounded cursor-pointer"
+                        >
+                            Guardar
+                        </button>
+                        </div>
+                    </>
+                    )}
+
+                    {/* Add Modal */}
+                    {modalType === 'add' && (
+                    <>
+                        <h2 className="text-xl font-bold mb-4">Nuevo Usuario</h2>
+                        <form
+                            onSubmit={async (e) => { // https://stackoverflow.com/questions/72788643/htmlformelement-null-is-not-assignable-to-parameter-of-type-string-typescript
+                                e.preventDefault(); // prevent default 
+
+                                const form = e.target as HTMLFormElement;
+                                const username = (form.username as HTMLInputElement).value; // name fields connects the data to the inputs
+                                const password = (form.password as HTMLInputElement).value;
+
+                                await axios.post(`${import.meta.env.VITE_API_URL}api/users/`, { username, password });
+                                const res = await axios.get(`${import.meta.env.VITE_API_URL}api/users/`);
+                                // fetchUsers()
+                                setUsers(res.data.data);
+
+                                setModalType(null); // close modal (maybe add a success gif?)
+                            }}
+                        >
+                        <input name="username" type="text" placeholder="Username" required className="border p-2 w-full mb-2" />
+                        <input name="password" type="password" placeholder="Password" required className="border p-2 w-full mb-4" />
+                        <div className="flex justify-end gap-4">
+                            <button type="button" onClick={() => setModalType(null)} className="px-4 py-2 bg-gray-300 rounded cursor-pointer">Cancelar</button>
+                            <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded cursor-pointer">Crear</button>
+                        </div>
+                        </form>
+                    </>
+                    )}
+                </div>
+            </div>
+            )}
+
         </section>
     )
 }
